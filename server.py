aaf39e643b68d54bc4591a16b411ed71a69ea235
Chianti Scarlett <chianti.scarlett@gmail.com>
2019-06-22 00:30:23 +0900

    ServerLinker basic sketch complete.

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
-# Server program
+import dropbox
+import json
+import sys
 
 
-class Dropbox:
-    def __init__(self):
-        pass
+class _Dropbox:
+    def __init__(self, keys):
+        self._key = keys[0]
 
     def initialize_server(self):
         """
@@ -28,3 +30,28 @@ class Dropbox:
         Upload folder and its contents to Dropbox server.
         """
         pass
+
+
+class ServerLinker:
+    """
+    Read settings.json file and load appropriate server class
+    """
+
+    def __init__(self):
+        self._servers = ['Dropbox']  # possible servers
+
+        # Read settings.json file
+        with open('settings.json', 'r') as fp:
+            self.settings = json.loads(fp.read())
+
+        # Validate SERVER_TYPE:
+        if self.settings['SERVER_TYPE'] not in self._servers:
+            self.raise_error(
+                'Invalid server type < {} >'
+                .format(self.settings['SERVER_TYPE']))
+        elif self.settings['SERVER_TYPE'] == 'Dropbox':
+            self.server = _Dropbox(self.settings['KEYS'])
+
+    def raise_error(self, message):
+        print('[*]', message)
+        sys.exit(0)
