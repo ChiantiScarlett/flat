45bb6bab85264050dde27009cdb4caa358b84d76
Chianti Scarlett <chianti.scarlett@gmail.com>
2019-06-22 00:00:03 +0900

    Class basic sketch complete.

--- server.py
+++ server.py
@@ -1,1 +1,30 @@
 # Server program
+
+
+class Dropbox:
+    def __init__(self):
+        pass
+
+    def initialize_server(self):
+        """
+        Initialize connection
+        """
+        pass
+
+    def upload_file(self):
+        """
+        Upload single file to Dropbox server.
+        """
+        pass
+
+    def download(self):
+        """
+        Download folder or file from Dropbox server.
+        """
+        pass
+
+    def upload_folder(self):
+        """
+        Upload folder and its contents to Dropbox server.
+        """
+        pass
